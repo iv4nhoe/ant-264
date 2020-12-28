13759c2714de08d51f492ec6eda8ffe74b6b0399
Klimonov <klimonov.dev@gmail.com>
2020-12-28 16:27:01 +0300

    add comments. rename payload to cargo

--- bot.cpp
+++ bot.cpp
@@ -9,24 +9,26 @@ using namespace std;
 using namespace httplib;
 using namespace nlohmann;
 
+// available actions and directions
 const vector<string> ACTIONS = { "stay", "move", "eat", "take", "put" };
 const vector<string> DIRECTIONS = { "up", "down", "right", "left" };
 
 int main(int argc, char* argv[])
 {
-  // Initialize random number generator
+  // initialize random number generator
   random_device rd;
   mt19937 mt(rd());
-  uniform_int_distribution<size_t> dist1(0, ACTIONS.size()-1);
-  uniform_int_distribution<size_t> dist2(0, DIRECTIONS.size()-1);
+  uniform_int_distribution<size_t> dist1(0, ACTIONS.size()-1); // pick random action from array on line 13
+  uniform_int_distribution<size_t> dist2(0, DIRECTIONS.size()-1); // pick random direction from array on line 14
 
-  // Initialize the http server
+  // initialize the http server
   Server svr;
+  // sim will make http post request to your bot
   svr.Post("/", [&](const Request& req, Response& res) {
-    // Hive object from request payload
+    // your bot respons should be json object
     auto hive = json::parse(req.body);
 
-    // Loop through ants and give orders
+    // loop through ants and give orders
     auto orders = std::vector<json>();
     for (auto& ant : hive["ants"].items()) {
       json order = {
@@ -34,6 +36,7 @@ int main(int argc, char* argv[])
         {"act", ACTIONS[dist1(mt)] },
         {"dir", DIRECTIONS[dist2(mt)]}
       };
+      // add order to your response object from line 32
       orders.push_back(order);
     }
 
@@ -41,6 +44,7 @@ int main(int argc, char* argv[])
     response["orders"]=orders;
 
     cout << response.dump() << '\n';
+    // finish your response and send back json to 
     res.set_content(response.dump(), "application/json");
     // json format sample:
     // {"orders": [
@@ -50,6 +54,13 @@ int main(int argc, char* argv[])
 
   });
 
+  // starting listen for http calls on port :7070
   svr.listen("0.0.0.0", 7070);
   return 0;
 }
+
+// this code available at https://github.com/anthive/cpp
+// to test it localy, submit post request with payload.json using postman or curl
+// curl -X 'POST' -d @payload.json http://localhost:7070
+
+// have fun!
